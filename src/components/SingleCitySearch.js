a3f31d54922cb40893a0fe9ef358cb340f990a16
Glkorded <maozedong-909@mail.ru>
2019-02-05 00:42:33 +0300

    Made debounce

--- src/components/SingleCitySearch.js
+++ src/components/SingleCitySearch.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import SingleCity from './SingleCity'
 import {Link} from 'react-router-dom'
+import {debounce} from "./debounce";
 
 class SingleCitySearch extends React.Component {
   constructor(props) {
@@ -11,42 +12,53 @@ class SingleCitySearch extends React.Component {
       mustFetch: false,
     }
   }
+
   handleChange = e => {
-    //  if (e.key === "Enter") {
     this.setState({
       searchTitle: e.target.value,
-      mustFetch: !this.state.mustFetch
-    })
-    //   }
+    });
+    if (this.state.mustFetch !== true) { //We divide this.setState to prevent setting mustFetch to true on every change in input
+      console.log("Must fetch status changed");
+      this.setState({
+        mustFetch: true
+      });
+    }
+    this.fetchMethod()
   };
 
   setNewData(data) {
     this.setState({
       data: data,
-      mustFetch: !this.state.mustFetch
-    })
+      mustFetch: false
+    });
+    console.log("New data set...")
   }
 
-  render() {
+  fetchMethod = debounce(() => {
     const proxy = 'https://cors-anywhere.herokuapp.com/';
     const url = 'https://www.metaweather.com/api';
     if (this.state.mustFetch) {
+      console.log("Debounce started...");
       fetch(`${proxy}${url}/location/search/?query=${this.state.searchTitle}`)
         .then(response => {
           if (response.ok) {
-            return response.json()
+            console.log("Response got...");
+            return response.json();
           }
         })
         .then(data => {
             if (data !== undefined) {
+              console.log("Data got...")
               this.setNewData(data)
             }
           }
         )
         .catch(error => error);
     }
-    console.log("Must fetch status is " + this.state.mustFetch);
+  }, 500);
 
+  render() {
+    console.log("Must fetch status is " + this.state.mustFetch);
     return (
       <div>
         <h2>
@@ -57,7 +69,7 @@ class SingleCitySearch extends React.Component {
         <input onChange={this.handleChange}/>
         <div>
           {this.state.data.map((single) =>
-            <div>
+            <div key = {single.woeid}>
             <SingleCity
               key = {single.woeid}
               title = {single.title}
---
+++ src/components/debounce.js
@@ -0,0 +1,13 @@
+export const debounce = (f, ms) => {
+  let timer = null;
+  return function (...args) {
+    const onComplete = () => {
+      f.apply(this, args);
+      timer = null;
+    };
+    if (timer) {
+      clearTimeout(timer);
+    }
+    timer = setTimeout(onComplete, ms);
+  };
+};
